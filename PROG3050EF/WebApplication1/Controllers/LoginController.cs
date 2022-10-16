ab1c8f1a643eb611a9d17d5e16165b83a555e162
rupomghosh <rghosh2388@conestogac.on.ca>
2022-10-16 00:57:11 -0400

    Login controller changed

--- PROG3050EF/WebApplication1/Controllers/LoginController.cs
+++ PROG3050EF/WebApplication1/Controllers/LoginController.cs
@@ -45,7 +45,7 @@ namespace WebApplication1.Controllers
                 _storeContext.Customers.Add(customer);
                 _storeContext.SaveChanges();
                 TempData["Message"] = customer.Nickname + " Added";
-                return RedirectToAction("Index", "Customer");
+                return RedirectToAction("Index", "Login");
             }
             else
             {
@@ -80,7 +80,7 @@ namespace WebApplication1.Controllers
                 _storeContext.Customers.Update(customer);
                 _storeContext.SaveChanges();
                 TempData["Message"] = customer.Nickname + " Edited";
-                return RedirectToAction("Index", "Customer");
+                return RedirectToAction("Index", "Login");
             }
             else
             {
@@ -110,7 +110,7 @@ namespace WebApplication1.Controllers
             _storeContext.Customers.Remove(customer);
             _storeContext.SaveChanges();
             TempData["Message"] = customer.Nickname + " Deleted";
-            return RedirectToAction("Index", "Customer");
+            return RedirectToAction("Index", "Login");
         }
         private StoreContext _storeContext;
     }
