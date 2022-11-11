3adb23dd7fce1c7f2fcde94456a00d026df3c220
Dmytrii Furs <furs.dmitry@gmail.com>
2022-11-10 23:47:30 -0500

    Pagination is added

---
+++ PROG3050EF/WebApplication1/TagHelpers/NumberDropDownTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;    // [ViewContext] attribute
+using Microsoft.AspNetCore.Mvc.Rendering;       // ViewContext data type
+using Microsoft.AspNetCore.Routing;             // LinkGenerator
+using GameStore.Models.Grid;
+using GameStore.Models.ExtensionModels;
+
+namespace GameStore.TagHelpers
+{
+    [HtmlTargetElement("select", Attributes = "my-min-number, my-max-number")]
+    public class NumberDropDownTagHelper : TagHelper
+    {
+        [HtmlAttributeName("my-min-number")]
+        public int Min { get; set; }
+        [HtmlAttributeName("my-max-number")]
+        public int Max { get; set; }
+
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            // get selected value from view's model
+            ModelExpression aspfor =
+                (ModelExpression)context.AllAttributes["asp-for"].Value;
+            int modelValue = (int)aspfor?.Model;
+
+            for (int i = Min; i <= Max; i++)
+            {
+                TagBuilder option = new TagBuilder("option");
+                option.InnerHtml.Append(i.ToString());
+
+                // mark option as selected if matches model’s value
+                if (modelValue == i)
+                    option.Attributes["selected"] = "selected";
+
+                output.Content.AppendHtml(option);
+            }
+        }
+    }
+}
---
+++ PROG3050EF/WebApplication1/TagHelpers/PagingLinkTagHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;    // [ViewContext] attribute
+using Microsoft.AspNetCore.Mvc.Rendering;       // ViewContext data type
+using Microsoft.AspNetCore.Routing;             // LinkGenerator
+using GameStore.Models.Grid;
+using GameStore.Models.ExtensionModels;
+
+namespace GameStore.TagHelpers
+{
+    [HtmlTargetElement("my-paging-link")]
+    public class PagingLinkTagHelper : TagHelper
+    {
+        private LinkGenerator linkBuilder;
+        public PagingLinkTagHelper(LinkGenerator lg) => linkBuilder = lg;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewCtx { get; set; }
+
+        public int Number { get; set; }
+        public RouteDictionary Current { get; set; }
+
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            // update routes for this paging link
+            var routes = Current.Clone();
+            routes.PageNumber = Number;
+
+            // get controller and action method, create paging link URL
+            string ctlr = ViewCtx.RouteData.Values["controller"].ToString();
+            string action = ViewCtx.RouteData.Values["action"].ToString();
+            string url = linkBuilder.GetPathByAction(action, ctlr, routes);
+
+            // build up CSS string
+            string linkClasses = "btn btn-outline-primary";
+            if (Number == Current.PageNumber)
+                linkClasses += " active";
+
+            // create link
+            output.BuildLink(url, linkClasses);
+            output.Content.SetContent(Number.ToString());
+        }
+    }
+}
